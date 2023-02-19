5c3a2f8ff8ebde1dc03e1df6739cd6809cf30be3
kaminami <ka.minami@nifty.com>
2023-02-20 02:08:44 +0900

    addStarMenuメソッドを利用した記述に変更

--- star_menu_example/lib/main.dart
+++ star_menu_example/lib/main.dart
@@ -46,9 +46,9 @@ class _Canvas extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: const [
-        _DraggableObject(name: 'circle', initialPosition: Offset(100, 100), iconData: Icons.circle),
-        _DraggableObject(name: 'circle_notifications_outlined', initialPosition: Offset(150, 100), iconData: Icons.circle_notifications_outlined),
-        _DraggableObject(name: 'account_circle', initialPosition: Offset(200, 100), iconData: Icons.account_circle),
+        _DraggableObject(name: 'Account', initialPosition: Offset(200, 100), iconData: Icons.account_circle),
+        _DraggableObject(name: 'Balance', initialPosition: Offset(100, 100), iconData: Icons.balance),
+        _DraggableObject(name: 'Clock', initialPosition: Offset(150, 100), iconData: Icons.lock_clock),
       ],
     );
   }
@@ -77,80 +77,55 @@ class _DraggableObjectState extends State<_DraggableObject> {
     icon = Icon(widget.iconData, size: 40);
   }
 
-  void _showMessage(String message, BuildContext context) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        duration: const Duration(seconds: 1),
-        content: Text(message),
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     StarMenuController menuController = StarMenuController();
 
-    List<Widget> menuItems = [
-      FloatingActionButton(
-        onPressed: () {
-          _showMessage('tapped: "add_call" menu, target: ${widget.name}', context);
-        },
-        backgroundColor: Colors.black,
-        child: const Icon(Icons.add_call),
-      ),
-      FloatingActionButton(
-        onPressed: () {
-          _showMessage('tapped: "adb menu", target: ${widget.name}', context);
-        },
-        backgroundColor: Colors.indigo,
-        child: const Icon(Icons.adb),
-      ),
-      FloatingActionButton(
-        onPressed: () {
-          _showMessage('tapped: "home menu", target: ${widget.name}', context);
-        },
-        backgroundColor: Colors.purple,
-        child: const Icon(Icons.home),
-      ),
-      FloatingActionButton(
-        onPressed: () {
-          _showMessage('tapped: "delete" menu, target: ${widget.name}', context);
-        },
-        backgroundColor: Colors.blueGrey,
-        child: const Icon(Icons.delete),
-      ),
-      FloatingActionButton(
-        onPressed: () {
-          _showMessage('tapped: "get_app" menu, target: ${widget.name}', context);
-        },
-        backgroundColor: Colors.deepPurple,
-        child: const Icon(Icons.get_app),
-      ),
+    List<Widget> menuItems = const [
+      FloatingActionButton(backgroundColor: Colors.blue, onPressed: null, child: Icon(Icons.add_call)),
+      FloatingActionButton(backgroundColor: Colors.green, onPressed: null, child: Icon(Icons.adb)),
+      FloatingActionButton(backgroundColor: Colors.purple, onPressed: null, child: Icon(Icons.home)),
+      FloatingActionButton(backgroundColor: Colors.brown, onPressed: null, child: Icon(Icons.delete)),
+      FloatingActionButton(backgroundColor: Colors.deepOrange, onPressed: null, child: Icon(Icons.get_app)),
     ];
 
     return Positioned(
       left: position.dx,
       top: position.dy,
-      child: StarMenu(
-          params: const StarMenuParameters(
-            useLongPress: false,
-            useTouchAsCenter: true,
-          ),
-          controller: menuController,
-          items: menuItems,
-          child: Draggable(
-            childWhenDragging: Container(),
-            onDragEnd: (DraggableDetails details) {
-              setState(() {
-                final renderBox = context.findAncestorRenderObjectOfType() as RenderBox;
-                final localOffset = renderBox.globalToLocal(details.offset);
+      child: Draggable(
+        childWhenDragging: Container(),
+        onDragEnd: (DraggableDetails details) {
+          setState(() {
+            final renderBox = context.findAncestorRenderObjectOfType() as RenderBox;
+            final localOffset = renderBox.globalToLocal(details.offset);
 
-                position = localOffset;
-              });
-            },
-            feedback: icon,
-            child: icon,
-          )),
+            position = localOffset;
+          });
+        },
+        feedback: icon,
+        child: icon,
+      ).addStarMenu(
+        items: menuItems,
+        params: const StarMenuParameters(
+          useLongPress: false,
+          useTouchAsCenter: true,
+        ),
+        controller: menuController,
+        onItemTapped: (index, controller) {
+          final selectedMenuItem = menuItems[index] as FloatingActionButton;
+          _showMessage("${widget.name}, menu index:$index, ", selectedMenuItem.backgroundColor!, context);
+        },
+      ),
+    );
+  }
+
+  void _showMessage(String message, Color backgroundColor, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 1),
+        backgroundColor: backgroundColor,
+        content: Text(message),
+      ),
     );
   }
 }
