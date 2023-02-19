1f5262009f78bea539eeca6631010b38f4948247
kaminami <ka.minami@nifty.com>
2023-02-20 01:38:55 +0900

    DraggableなWidgetに対して、タップでcircleメニューが利用できるところまで

--- star_menu_example/lib/main.dart
+++ star_menu_example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:star_menu/star_menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +8,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'StarMenu Test App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'StarMenu Test'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +30,127 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _Canvas(),
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _Canvas extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: const [
+        _DraggableObject(name: 'circle', initialPosition: Offset(100, 100), iconData: Icons.circle),
+        _DraggableObject(name: 'circle_notifications_outlined', initialPosition: Offset(150, 100), iconData: Icons.circle_notifications_outlined),
+        _DraggableObject(name: 'account_circle', initialPosition: Offset(200, 100), iconData: Icons.account_circle),
+      ],
+    );
+  }
+}
+
+class _DraggableObject extends StatefulWidget {
+  final Offset initialPosition;
+  final IconData iconData;
+  final String name;
+
+  const _DraggableObject({required this.initialPosition, required this.iconData, required this.name});
+
+  @override
+  State<_DraggableObject> createState() => _DraggableObjectState();
+}
+
+class _DraggableObjectState extends State<_DraggableObject> {
+  late Offset position;
+  late Icon icon;
+
+  @override
+  void initState() {
+    super.initState();
+
+    position = widget.initialPosition;
+    icon = Icon(widget.iconData, size: 40);
+  }
+
+  void _showMessage(String message, BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 1),
+        content: Text(message),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    StarMenuController menuController = StarMenuController();
+
+    List<Widget> menuItems = [
+      FloatingActionButton(
+        onPressed: () {
+          _showMessage('tapped: "add_call" menu, target: ${widget.name}', context);
+        },
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.add_call),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      FloatingActionButton(
+        onPressed: () {
+          _showMessage('tapped: "adb menu", target: ${widget.name}', context);
+        },
+        backgroundColor: Colors.indigo,
+        child: const Icon(Icons.adb),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      FloatingActionButton(
+        onPressed: () {
+          _showMessage('tapped: "home menu", target: ${widget.name}', context);
+        },
+        backgroundColor: Colors.purple,
+        child: const Icon(Icons.home),
+      ),
+      FloatingActionButton(
+        onPressed: () {
+          _showMessage('tapped: "delete" menu, target: ${widget.name}', context);
+        },
+        backgroundColor: Colors.blueGrey,
+        child: const Icon(Icons.delete),
+      ),
+      FloatingActionButton(
+        onPressed: () {
+          _showMessage('tapped: "get_app" menu, target: ${widget.name}', context);
+        },
+        backgroundColor: Colors.deepPurple,
+        child: const Icon(Icons.get_app),
+      ),
+    ];
+
+    return Positioned(
+      left: position.dx,
+      top: position.dy,
+      child: StarMenu(
+          params: const StarMenuParameters(
+            useLongPress: false,
+            useTouchAsCenter: true,
+          ),
+          controller: menuController,
+          items: menuItems,
+          child: Draggable(
+            childWhenDragging: Container(),
+            onDragEnd: (DraggableDetails details) {
+              setState(() {
+                final renderBox = context.findAncestorRenderObjectOfType() as RenderBox;
+                final localOffset = renderBox.globalToLocal(details.offset);
+
+                position = localOffset;
+              });
+            },
+            feedback: icon,
+            child: icon,
+          )),
     );
   }
 }
