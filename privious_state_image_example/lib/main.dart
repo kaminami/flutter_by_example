eec6c73b961761f42053ee3f9d8a71f6c648774f
kaminami <ka.minami@nifty.com>
2023-02-20 08:34:38 +0900

    タイトル設定

--- privious_state_image_example/lib/main.dart
+++ privious_state_image_example/lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Draggable Test App',
+      title: 'Previous State Image Test App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Draggable Test'),
+      home: const MyHomePage(title: 'Previous State Image Test'),
     );
   }
 }
