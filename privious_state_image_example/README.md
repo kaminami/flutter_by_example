a8cbac9aaa95018f4e38c5338828f73b3a3998ce
kaminami <ka.minami@nifty.com>
2023-02-21 04:09:10 +0900

    ひととおり実装。

--- privious_state_image_example/README.md
+++ privious_state_image_example/README.md
@@ -1,2 +1,3 @@
 # privious_state_image_example
 
+直前の画面状態を、背景画像として設定するサンプルアプリ。
--- privious_state_image_example/lib/main.dart
+++ privious_state_image_example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Previous State Image Test App',
+      title: 'Previous State Image Example App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Previous State Image Test'),
+      home: const MyHomePage(title: 'Previous State Image Example'),
     );
   }
 }
@@ -29,14 +30,63 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Widget backgroundImage = Image.asset('assets/images/blank.png'); // RawImageと差し替えるので、Widgetクラスで受ける
+
+  final GlobalKey canvasBoundaryKey = GlobalKey();
+  late Widget canvasBoundary;
+
+  @override
+  void initState() {
+    super.initState();
+
+    canvasBoundary = RepaintBoundary(
+      key: canvasBoundaryKey,
+      child: _Canvas(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: _Canvas(),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            alignment: Alignment.topCenter,
+            // color: Colors.lightGreen,
+            child: Opacity(
+              opacity: 0.3,
+              child: backgroundImage,
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.transparent,
+            child: canvasBoundary,
+          ),
+          FloatingActionButton(
+              backgroundColor: Colors.deepOrange,
+              child: const Icon(Icons.camera_alt_outlined),
+              onPressed: () {
+                setState(() {
+                  updateCanvasBackground();
+                });
+              }),
+        ],
+      ),
     );
+  }
+
+  void updateCanvasBackground() {
+    RenderRepaintBoundary boundary = canvasBoundaryKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    final snapshot = boundary.toImageSync();
+
+    backgroundImage = RawImage(image: snapshot);
   }
 }
 
@@ -45,9 +95,9 @@ class _Canvas extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: const [
-        _DraggableObject(initialPosition: Offset(100, 100), iconData: Icons.circle),
-        _DraggableObject(initialPosition: Offset(150, 100), iconData: Icons.circle_notifications_outlined),
-        _DraggableObject(initialPosition: Offset(200, 100), iconData: Icons.account_circle),
+        _DraggableObject(name: 'Account', initialPosition: Offset(200, 100), iconData: Icons.account_circle),
+        _DraggableObject(name: 'Balance', initialPosition: Offset(100, 100), iconData: Icons.balance),
+        _DraggableObject(name: 'Clock', initialPosition: Offset(150, 100), iconData: Icons.lock_clock),
       ],
     );
   }
@@ -56,8 +106,9 @@ class _Canvas extends StatelessWidget {
 class _DraggableObject extends StatefulWidget {
   final Offset initialPosition;
   final IconData iconData;
+  final String name;
 
-  const _DraggableObject({required this.initialPosition, required this.iconData});
+  const _DraggableObject({required this.initialPosition, required this.iconData, required this.name});
 
   @override
   State<_DraggableObject> createState() => _DraggableObjectState();
